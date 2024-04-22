fef9d4c45bce608acf250317c0b3b9657844c3b9
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-22 12:12:13 +0000

    Piscine Rust

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,25 +1,16 @@
-pub fn sum(a: u8, b: u8) -> String {
-    if let Some(c) = a.checked_add(b) {
-        c.to_string()
-    } else {
-        String::from("ERROR: attempt to add with overflow")
-    }
+pub fn sum(a: u8, b: u8) -> u8 {
+    let c : u8 = a + b;
+    c
 }
 
-pub fn diff(a: i16, b: i16) -> String {
-    if let Some(c) = a.checked_sub(b) {
-        c.to_string()
-    } else {
-        String::from("ERROR: attempt to add with overflow")
-    }
+pub fn diff(a: i16, b: i16) -> i16 {
+    let c : i16 = a - b;
+    c
 }
 
-pub fn pro(a: i8, b: i8) -> String {
-    if let Some(c) = a.checked_mul(b) {
-        c.to_string()
-    } else {
-        String::from("ERROR: attempt to add with overflow")
-    }
+pub fn pro(a: i8, b: i8) -> i8 {
+    let c : i8 = a * b;
+    c
 }
 
 pub fn quo(a: f32, b: f32) -> f32 {
@@ -31,17 +22,17 @@ pub fn rem(a: f32, b: f32) -> f32 {
 }
 
 
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
