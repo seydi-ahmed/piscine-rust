b046c90a7855e4f600dddf8da6054d990408ef42
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-23 13:03:59 +0000

    Piscine Rust

---
+++ to_url/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn to_url(s: &str) -> String {
+
+    // let mut mot = String::new();
+
+    // for letter in s.chars(){
+    //     if letter == ' '{
+    //         letter =  '%';
+    //         letter =  '2';
+    //         letter =  '0';
+    //         // mot += "%20";
+    //     }
+    // }
+
+    // s.
+
+    s.replace(' ', "%20")
+}
