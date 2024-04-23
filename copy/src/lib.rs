5c5729b19896ce4d5b282655d80ee45bf02ce791
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-23 17:48:41 +0000

    Piscine Rust

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -3,7 +3,17 @@ pub fn nbr_function(c: i32) -> (i32, f64, f64) {
 }
 
 pub fn str_function(a: String) -> (String, String) {
-    let second = a.parse()::<f64>
+    let mut second = String::new();
+
+    for number in a.split_whitespace() {
+        let number_i: f64 = number.parse().unwrap();
+        let number_i_exp = number_i.exp();
+
+        second.push_str(&number_i_exp.to_string());
+        second.push_str(" ");
+    }
+
+    (a, String::from(second.trim()))
 }
 
 // pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
--- copy/src/main.rs
+++ copy/src/main.rs
@@ -3,7 +3,7 @@ use copy::*;
 fn main() {
     let a: i32 = 0;
     let b = String::from("1 2 4 5 6");
-    // let c = vec![1, 2, 4, 5];
+    let c = vec![1, 2, 4, 5];
 
     println!("{:?}", nbr_function(a));
     println!("{:?}", str_function(b));
