11c9322d040f4c350276e768553593764c8d2bce
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-23 18:07:07 +0000

    Piscine Rust

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn nbr_function(c: i32) -> (i32, f64, f64) {
-    (c, (c as f64).exp(), (c as f64).ln())
+    (c, (c as f64).exp(), (c as f64).log10())
 }
 
 pub fn str_function(a: String) -> (String, String) {
