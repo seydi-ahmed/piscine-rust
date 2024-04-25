991e20113386e7da1074f129c6673e4372f9dafe
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-25 17:44:15 +0000

    Piscine Rust

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,25 +1,28 @@
 use std::collections::HashMap;
 
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
-    let mut s1_vec: Vec<String> = Vec::new();
-    let mut s2_vec: Vec<String> = Vec::new();
+    let mut s1_vec: Vec<char> = Vec::new();
+    let mut s2_vec: Vec<char> = Vec::new();
 
     for ss1 in s1.chars(){
-        s1_vec.push(ss1.to_string());
+        s1_vec.push(ss1);
     }
     for ss2 in s2.chars(){
-        s2_vec.push(ss2.to_string());
+        s2_vec.push(ss2);
     }
 
-    let mut hash_map_s1: HashMap<&str, usize> = HashMap::new();
-    let mut hash_map_s2: HashMap<&str, usize> = HashMap::new();
+    let hash_map_s1: HashMap<char, usize> = HashMap::from(word_frequency_counter(s1_vec));
+    let hash_map_s2: HashMap<char, usize> = HashMap::from(word_frequency_counter(s2_vec));
 
-    
+    // hash_map_s1 = word_frequency_counter(s1_vec);
+    // hash_map_s2 = word_frequency_counter(s2_vec);
+
+    hash_map_s1 == hash_map_s2
 }
 
-pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
-    let mut tab_deja_verfie: Vec<&str> = Vec::new();
-    let mut hash_map: HashMap<&str, usize> = HashMap::new();
+pub fn word_frequency_counter(words: Vec<char>) -> HashMap<char, usize> {
+    let mut tab_deja_verfie: Vec<char> = Vec::new();
+    let mut hash_map: HashMap<char, usize> = HashMap::new();
 
     for word in words.clone() {
         let mut nombre_de_fois: usize = 0;
@@ -38,7 +41,7 @@ pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
     hash_map
 }
 
-pub fn in_the_vec(words: Vec<&str>, word: &str) -> bool {
+pub fn in_the_vec(words: Vec<char>, word: char) -> bool {
     let mut index: usize = 0;
     for mot in words {
         if mot == word {
