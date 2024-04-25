bccc13805f5aa56af6f88f86beb2b62d3b7e35fd
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-25 10:07:16 +0000

    Piscine Rust

--- changes/src/lib.rs
+++ changes/src/lib.rs
@@ -1,4 +1,5 @@
 #[derive(Debug, Eq, PartialEq, Clone)]
+
 pub struct Light {
 	pub alias: String,
 	pub brightness: u8,
@@ -6,8 +7,14 @@ pub struct Light {
 
 impl Light {
 	pub fn new(alias: &str) -> Self {
+		let lumiere = Light {
+		alias : alias.to_string(),
+		brightness : 0,
+	};
+
+		lumiere
 	}
 }
 
-pub fn change_brightness(lights: &mut Vec<Light>, alias: &str, value: u8) {
-}
+// pub fn change_brightness(lights: &mut Vec<Light>, alias: &str, value: u8) {
+// }
--- changes/src/main.rs
+++ changes/src/main.rs
@@ -2,12 +2,14 @@ use changes::*;
 
 fn main() {
     // bedroom
-    let mut lights = vec![
-        Light::new("living_room"),
-        Light::new("bedroom"),
-        Light::new("rest_room"),
-    ];
-    println!("brightness = {}", lights[0].brightness);
-    change_brightness(&mut lights, "living_room", 200);
-    println!("new brightness = {}", lights[0].brightness);
+    // let mut lights = vec![
+    //     Light::new("living_room"),
+    //     Light::new("bedroom"),
+    //     Light::new("rest_room"),
+    // ];
+    // println!("brightness = {}", lights[0].brightness);
+    // change_brightness(&mut lights, "living_room", 200);
+    // println!("new brightness = {}", lights[0].brightness);
+
+    println!("{:?}", Light::new("living_room"));
 }
