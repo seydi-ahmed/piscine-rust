25e4493d2042aed8b21811abb29eeea9486586a6
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-29 17:45:37 +0000

    Piscine Rust

---
+++ cipher/src/lib.rs
@@ -0,0 +1,43 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CipherError {
+    pub validation: bool,
+    pub expected: String,
+}
+
+impl CipherError {
+    pub fn new(validation: bool, expected: String) -> CipherError {
+        CipherError { validation, expected }
+    }
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Option<Result<bool, CipherError>> {
+    if original.len() != ciphered.len() {
+        return Some(Err(CipherError::new(false, ciphered.to_string())));
+    }
+
+    let mut result = true;
+    for (orig_char, ciphered_char) in original.chars().zip(ciphered.chars()) {
+        if atbash(orig_char) != ciphered_char {
+            result = false;
+            break;
+        }
+    }
+
+    if result {
+        Some(Ok(true))
+    } else {
+        Some(Err(CipherError::new(false, ciphered.to_string())))
+    }
+}
+
+fn atbash(c: char) -> char {
+    match c {
+        'a'..='z' => ('z' as u8 - (c as u8 - 'a' as u8)) as char,
+        'A'..='Z' => ('Z' as u8 - (c as u8 - 'A' as u8)) as char,
+        _ => c,
+    }
+}
+
+// Some(Ok(true))
+// Some(Err(CipherError { validation: false, expected: "1Svool 2dliow!" }))
+// None
