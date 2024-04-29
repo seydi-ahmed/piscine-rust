fd5f06f5ce61eb6618b324e7832f5c20d33aa402
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-29 16:14:27 +0000

    Piscine Rust

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -8,7 +8,7 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<String, String>, security_level: Security) -> String {
     match security_level {
-        Security::Unknown => server.expect(""),
+        Security::Unknown => server.unwrap(),
         Security::High => {
             if let Err(_err) = server {
                 panic!("ERROR: program stops");
