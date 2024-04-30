85a0fa9820a38fd1b3144c8532a431d63b7b3a13
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-30 20:25:30 +0000

    Piscine Rust

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,20 +1,20 @@
-use chrono::{Datelike, NaiveDate, Weekday};
+pub use chrono::{Datelike, NaiveDate, Weekday};
 
 pub fn middle_day(year: i32) -> Option<Weekday> {
-    // Vérifier si l'année est bissextile
-    let is_leap_year = NaiveDate::from_ymd_opt(year, 2, 29).is_some();
+    let days_in_year = if NaiveDate::from_ymd_opt(year,2,29).is_some(){
+        366
+    } else {
+        365
+    };
 
-    // Si l'année est bissextile ou non
-    let days_in_year = if is_leap_year { 366 } else { 365 };
-
-    // Si le nombre de jours dans l'année est pair, il n'y a pas de jour médian
     if days_in_year % 2 == 0 {
         return None;
+    } else {
+        let middleDay = (days_in_year+1)/2;
+        let middleDate = match NaiveDate::from_ymd_opt(year, middleDay as u32){
+            Some(d) => d,
+            None => panic!("Invalid Date"),
+        };
+        Some(middleDate.weekday())
     }
-
-    // Calculer la date médiane de l'année
-    let middle_date = NaiveDate::from_ymd_opt(year, 1, 1)?.checked_add_signed(chrono::Duration::days((days_in_year / 2) as i64));
-
-    // Extraire le jour de la semaine de la date médiane, s'il existe
-    middle_date.map(|date| date.weekday())
 }
