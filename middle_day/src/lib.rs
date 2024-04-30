8d4a29b7b1ebd2bbf06b4d7adbcee855829ec2d8
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-30 20:32:20 +0000

    Piscine Rust

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,20 +1,20 @@
 pub use chrono::{Datelike, NaiveDate, Weekday};
 
 pub fn middle_day(year: i32) -> Option<Weekday> {
-    let days_in_year = if NaiveDate::from_ymd_opt(year,2,29).is_some(){
+    let jours_dans_annee = if NaiveDate::from_ymd_opt(year,2,29).is_some(){
         366
     } else {
         365
     };
 
-    if days_in_year % 2 == 0 {
+    if jours_dans_annee % 2 == 0 {
         return None;
     } else {
-        let middleday = (days_in_year+1)/2;
-        let middle_date = match NaiveDate::from_yo_opt(year, middleday as u32){
+        let jours_median_annee = (jours_dans_annee+1)/2;
+        let date_median = match NaiveDate::from_yo_opt(year, jours_median_annee as u32){
             Some(d) => d,
             None => panic!("Invalid Date"),
         };
-        Some(middle_date.weekday())
+        Some(date_median.weekday())
     }
 }
