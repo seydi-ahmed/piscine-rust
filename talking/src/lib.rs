34af01b2ffb09b3149b91d4bdc76ab30aeb8091b
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-01 22:47:43 +0000

    Piscine Rust

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,28 +1,3 @@
-// pub fn talking(text: &str) -> &str {
-//     // Check if the text is empty
-//     if text.trim().is_empty() {
-//         return "Just say something!";
-//     }
-
-//     if text == text.to_uppercase() {
-//         // Check if the text is a question (ends with a question mark)
-//         if text.ends_with('?') {
-//             return "Quiet, I am thinking!";
-//         } else {
-//             return "There is no need to yell, calm down!";
-//         }
-//     }
-
-//     // Check if the text is a question
-//     if text.trim_end().ends_with('?') {
-//         return "Sure.";
-//     }
-
-//     // Default response
-//     "Interesting"
-// }
-
-
 // "There is no need to yell, calm down!"
 // "Sure."
 // "Quiet, I am thinking!"
@@ -46,9 +21,13 @@ pub fn talking(text: &str) -> &str {
     }
 
     // Check if the text is a question
-    if text.trim_end().ends_with('?') {
-        return "Sure.";
+    for letter in text.chars(){
+        if letter == '?'{
+            return "Sure.";
+        }
     }
+    // if text.ends_with('?') {
+    // }
 
     // Default response
     "Interesting"
