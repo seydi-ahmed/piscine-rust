c2a5c02129cf4bf6e7ae800affb7cc0b75464bfc
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-01 20:29:12 +0000

    Piscine Rust

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -1,6 +1,4 @@
-use serde_json::json;
-use serde_json::Value;
-use std::str::FromStr;
+use json::*;
 
 pub struct Food {
     pub name: String,
@@ -11,48 +9,30 @@ pub struct Food {
     pub nbr_of_portions: f64,
 }
 
-pub fn calculate_macros(foods: Vec<Food>) -> Value {
-    let mut total_calories = 0.0;
+pub fn calculate_macros(foods: Vec<Food>) -> JsonValue {
+    let mut total_cals = 0.0;
     let mut total_carbs = 0.0;
     let mut total_proteins = 0.0;
     let mut total_fats = 0.0;
 
-    for food in foods {
-        // Parse calorie values from strings to f64
-        let calorie_kj = f64::from_str(&food.calories[0][..food.calories[0].len() - 2])
-            .expect("Failed to parse calorie value");
-        let calorie_kcal = f64::from_str(&food.calories[1][..food.calories[1].len() - 4])
-            .expect("Failed to parse calorie value");
-
-        // Convert kJ to kcal and sum up
-        total_calories += (calorie_kj / 4.184 + calorie_kcal) * food.nbr_of_portions;
-
-        // Calculate total macros considering portions
-        total_proteins += food.proteins * food.nbr_of_portions;
-        total_fats += food.fats * food.nbr_of_portions;
-        total_carbs += food.carbs * food.nbr_of_portions;
+    for fd in foods {
+        let cl = fd.calories[1].as_str().split("k").collect::<Vec<_>>()[0];
+        let cl_num : f64 = cl.parse().unwrap_or(0.0);
+        total_cals += cl_num * fd.nbr_of_portions;
+        total_proteins += fd.proteins * fd.nbr_of_portions;
+        total_fats += fd.fats * fd.nbr_of_portions;
+        total_carbs += fd.carbs * fd.nbr_of_portions;
     }
 
-    // Round the totals to two decimal places
-    let total_calories_rounded = (total_calories * 100.0).round() / 100.0;
-    let total_proteins_rounded = (total_proteins * 100.0).round() / 100.0;
-    let total_fats_rounded = (total_fats * 100.0).round() / 100.0;
-    let total_carbs_rounded = (total_carbs * 100.0).round() / 100.0;
-
-    // Build the JSON object
-    json!({
-        "cals": total_calories_rounded,
-        "carbs": total_carbs_rounded,
-        "proteins": total_proteins_rounded,
-        "fats": total_fats_rounded,
-    })
+    object! {
+        "cals": (total_cals*100.0).round()/100.0,
+        "carbs": (total_carbs*100.0).round()/100.0,
+        "proteins": (total_proteins*100.0).round()/100.0,
+        "fats": (total_fats*100.0).round()/100.0,
+    }
 }
-
-
-
 
     // "cals": 2777.39,
     // "carbs": 322.44,
     // "proteins": 122.06,
     // "fats": 106.93
-
