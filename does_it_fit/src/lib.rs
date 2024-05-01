e1c6d061a0f45664c1e01ad3a7c7a4799213e087
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-01 19:32:18 +0000

    Piscine Rust

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -4,27 +4,31 @@ pub use areas_volumes::*;
 pub fn area_fit(
     x: usize,
     y: usize,
-    objects: areas_volumes::GeometricalShapes,
+    objects: GeometricalShapes,
     times: usize,
     a: usize,
     b: usize,
 ) -> bool {
     match objects {
-        areas_volumes::GeometricalShapes::Square => {
-            let area_of_one = areas_volumes::square_area(a);
+        GeometricalShapes::Square => {
+            let square_area = crate::areas_volumes::square_area(a);
             let total_area = x * y;
-            total_area / area_of_one >= times
+            square_area * times <= total_area
         }
-        areas_volumes::GeometricalShapes::Triangle => {
-            let area_of_one = areas_volumes::triangle_area(a, b);
+        GeometricalShapes::Circle => {
+            let circle_area = crate::areas_volumes::circle_area(a);
             let total_area = x * y;
-            total_area as f64 / area_of_one >= times as f64
+            circle_area * times as f64 <= total_area as f64
         }
-        areas_volumes::GeometricalShapes::Circle => false,
-        areas_volumes::GeometricalShapes::Rectangle => {
-            let area_of_one = areas_volumes::rectangle_area(a, b);
+        GeometricalShapes::Rectangle => {
+            let rectangle_area = crate::areas_volumes::rectangle_area(a, b);
             let total_area = x * y;
-            total_area / area_of_one >= times
+            rectangle_area * times <= total_area
+        }
+        GeometricalShapes::Triangle => {
+            let triangle_area = crate::areas_volumes::triangle_area(a, b);
+            let total_area = x * y;
+            triangle_area * times as f64 <= total_area as f64
         }
     }
 }
@@ -33,35 +37,41 @@ pub fn volume_fit(
     x: usize,
     y: usize,
     z: usize,
-    objects: areas_volumes::GeometricalVolumes,
+    objects: GeometricalVolumes,
     times: usize,
     a: usize,
-    _b: usize,
-    _c: usize,
+    b: usize,
+    c: usize,
 ) -> bool {
     match objects {
-        areas_volumes::GeometricalVolumes::Cube => {
-            let volume_of_one = areas_volumes::cube_volume(a);
+        GeometricalVolumes::Cube => {
+            let cube_volume = crate::areas_volumes::cube_volume(a);
             let total_volume = x * y * z;
-            total_volume / volume_of_one >= times
+            cube_volume * times <= total_volume
         }
-        areas_volumes::GeometricalVolumes::Sphere => {
-            let volume_of_one = areas_volumes::sphere_volume(a);
+        GeometricalVolumes::Sphere => {
+            let sphere_volume = crate::areas_volumes::sphere_volume(a);
             let total_volume = x * y * z;
-            times as f64 <= (total_volume as f64 / volume_of_one)
+            sphere_volume * times as f64 <= total_volume as f64
         }
-        areas_volumes::GeometricalVolumes::Cone => false,
-        areas_volumes::GeometricalVolumes::Pyramid => false,
-        areas_volumes::GeometricalVolumes::Parallelepiped => {
-            let volume_of_one = areas_volumes::parallelepiped_volume(a, _b, _c);
+        GeometricalVolumes::Cone => {
+            let cone_volume = crate::areas_volumes::cone_volume(a, b);
             let total_volume = x * y * z;
-            total_volume / volume_of_one >= times
+            cone_volume * times as f64 <= total_volume as f64
+        }
+        GeometricalVolumes::Pyramid => {
+            let pyramid_volume = crate::areas_volumes::triangular_pyramid_volume(
+                crate::areas_volumes::triangle_area(a, b),
+                c,
+            );
+            let total_volume = x * y * z;
+            pyramid_volume * times as f64 <= total_volume as f64
+        }
+        GeometricalVolumes::Parallelepiped => {
+            let parallelepiped_volume =
+                crate::areas_volumes::parallelepiped_volume(a, b, c);
+            let total_volume = x * y * z;
+            parallelepiped_volume * times <= total_volume
         }
     }
 }
-
-
-// Do 100 rectangles (2x1) fit in a 2 by 4 square? false
-// Do 3 triangles (5 base and 3 height) fit in a 5 by 5 square? true
-// Do 3 spheres (2 radius) fit in a 5 by 5 by 5 box? true
-// Does 1 parallelepiped (6 base, 7 height and depth 4) fit in a 5 by 7 by 5 parallelepiped? true
