7d4ea892dd1fde6f3fdddcbd9746d48ce571496d
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-07 21:13:04 +0000

    Piscine Rust

--- drop_the_thread/src/lib.rs
+++ drop_the_thread/src/lib.rs
@@ -1,14 +1,57 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::cell::{RefCell, Cell};
+
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
+pub struct Workers {
+    pub drops: Cell<usize>,
+    pub states: RefCell<Vec<bool>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Workers {
+    pub fn new() -> Workers {
+        Workers {
+            drops: Cell::new(0),
+            states: RefCell::new(Vec::new()),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn new_worker(&self, cmd: String) -> (usize, Thread) {
+        let id = self.track_worker();
+        self.states.borrow_mut().push(false);
+        (id, Thread::new_thread(id, cmd, self))
+    }
+
+    pub fn track_worker(&self) -> usize {
+        self.states.borrow().len()
+    }
+
+    pub fn is_dropped(&self, id: usize) -> bool {
+        *self.states.borrow().get(id).unwrap_or(&true)
+    }
+
+    pub fn add_drop(&self, id: usize) {
+        let mut states = self.states.borrow_mut();
+        if *states.get(id).unwrap_or(&true) {
+            panic!("{} is already dropped", id);
+        } else {
+            states[id] = true;
+            self.drops.set(self.drops.get() + 1);
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Thread<'a> {
+    pid: usize,
+    cmd: String,
+    parent: &'a Workers,
+}
+
+impl<'a> Thread<'a> {
+    pub fn new_thread(pid: usize, cmd: String, parent: &'a Workers) -> Thread<'a> {
+        Thread { pid, cmd, parent }
+    }
+
+    pub fn skill(self) {
+        self.parent.add_drop(self.pid);
     }
 }
