2fc3ea4647148d130701bfa9009bce2ee2bfb08f
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-08 15:42:57 +0000

    Piscine Rust

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -12,7 +12,6 @@ pub trait AppendStr {
 impl AppendStr for StringValue {
     fn append_str(&mut self, str_to_append: String) {
         self.value.push_str(&str_to_append);
-        // return self.clone();
     }
 
     fn append_number(&mut self, nb_to_append: f64) {
@@ -23,12 +22,35 @@ impl AppendStr for StringValue {
         }
 
         self.value.push_str(&new_nb.to_string());
-        // return self.clone();
     }
 
     fn remove_punctuation_marks(&mut self) {
         let punctuation_marks = ['.', ',', '?', '!'];
         self.value.retain(|c| !punctuation_marks.contains(&c));
-        // return self.clone();
     }
 }
+
+
+// impl AppendStr for StringValue {
+//     fn append_str(&mut self, str_to_append: String) -> Self{
+//         self.value.push_str(&str_to_append);
+//         return self.value;
+//     }
+
+//     fn append_number(&mut self, nb_to_append: f64) -> Self {
+//         let mut new_nb : String = nb_to_append.clone().to_string();
+
+//         if nb_to_append.to_string().starts_with('-'){
+//             new_nb.remove(0);
+//         }
+
+//         self.value.push_str(&new_nb.to_string());
+//         return self.value;
+//     }
+
+//     fn remove_punctuation_marks(&mut self) -> Self{
+//         let punctuation_marks = ['.', ',', '?', '!'];
+//         self.value.retain(|c| !punctuation_marks.contains(&c));
+//         return self.value;
+//     }
+// }
