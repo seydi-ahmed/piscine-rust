caaf538888fd1c2c9a9a7898daef3fe68aa9f92b
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-08 15:20:44 +0000

    easy list

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -10,15 +10,15 @@ pub trait AppendStr {
 }
 
 impl AppendStr for StringValue {
-    fn append_str(&mut self, str_to_append: String) {
+    fn append_str(&mut self, str_to_append: String) -> Self {
         self.value.push_str(&str_to_append);
     }
 
-    fn append_number(&mut self, nb_to_append: f64) {
+    fn append_number(&mut self, nb_to_append: f64) -> Self {
         self.value.push_str(&nb_to_append.to_string());
     }
 
-    fn remove_punctuation_marks(&mut self) {
+    fn remove_punctuation_marks(&mut self) -> Self {
         let punctuation_marks = ['.', ',', '?', '!'];
         self.value.retain(|c| !punctuation_marks.contains(&c));
     }
