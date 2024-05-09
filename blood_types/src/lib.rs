f1b1082e1ca8228e3065c8e98665e984baeaead6
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-09 15:11:33 +0000

    Piscine Rust

--- blood_types/src/lib.rs
+++ blood_types/src/lib.rs
@@ -1,4 +1,28 @@
-// lib.rs
+use std::cmp::{Ord, Ordering};
+use std::fmt::{self, Debug, Display};
+use std::str::FromStr;
+
+#[derive(Debug, Clone)]
+pub struct ParseAntigenError;
+#[derive(Debug, Clone)]
+pub struct ParseRhFactorError;
+#[derive(Debug, Clone)]
+pub struct ParseBloodTypeError;
+impl fmt::Display for ParseBloodTypeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid blood type")
+    }
+}
+impl fmt::Display for ParseAntigenError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid antigen")
+    }
+}
+impl fmt::Display for ParseRhFactorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid rhfactor")
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, PartialOrd, Ord)]
 pub enum Antigen {
@@ -7,11 +31,29 @@ pub enum Antigen {
     B,
     O,
 }
+impl Display for Antigen {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Antigen::A => write!(f, "A"),
+            Antigen::AB => write!(f, "AB"),
+            Antigen::B => write!(f, "B"),
+            Antigen::O => write!(f, "0"),
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum RhFactor {
     Positive,
     Negative,
+}
+impl Display for RhFactor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RhFactor::Positive => write!(f, "+"),
+            RhFactor::Negative => write!(f, "-"),
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, PartialOrd)]
@@ -20,206 +62,160 @@ pub struct BloodType {
     pub rh_factor: RhFactor,
 }
 
-use std::cmp::{Ord, Ordering};
-
-use std::str::FromStr;
-
 impl FromStr for Antigen {
-    type Err = String;
+    type Err = ParseAntigenError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "A" => Ok(Antigen::A),
-            "AB" => Ok(Antigen::AB),
             "B" => Ok(Antigen::B),
+            "AB" => Ok(Antigen::AB),
             "O" => Ok(Antigen::O),
-            _ => Err("Invalid antigen".to_string()),
+            _ => Err(ParseAntigenError),
         }
     }
 }
 
 impl FromStr for RhFactor {
-    type Err = String;
+    type Err = ParseRhFactorError;
+    // type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "+" => Ok(RhFactor::Positive),
             "-" => Ok(RhFactor::Negative),
-            _ => Err("Invalid Rh factor".to_string()),
+            _ => Err(ParseRhFactorError),
         }
+    }
+}
+
+impl FromStr for BloodType {
+    // type Err = ParseBloodTypeError;
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let antigen = s[..s.len() - 1]
+            .parse::<Antigen>()
+            .map_err(|_| ParseAntigenError)
+            .unwrap();
+        let rh = s[s.len() - 1..]
+            .parse::<RhFactor>()
+            .map_err(|_| ParseRhFactorError)
+            .unwrap();
+
+        Ok(BloodType {
+            antigen,
+            rh_factor: rh,
+        })
     }
 }
 
 impl Ord for BloodType {
     fn cmp(&self, other: &Self) -> Ordering {
-        if self.antigen != other.antigen {
-            self.antigen.cmp(&other.antigen)
-        } else {
-            self.rh_factor.cmp(&other.rh_factor)
-        }
+        (&self.antigen, &self.rh_factor).cmp(&(&other.antigen, &other.rh_factor))
     }
 }
-
-// impl FromStr for BloodType {
-//     type Err = String;
-
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
-//         let parts: Vec<&str> = s.split("").collect();
-//         if parts.len() != 3 || (parts[2] != "+" && parts[2] != "-") {
-//             return Err("Invalid blood type format. Expected format: Antigen+/-".to_string());
-//         }
-
-//         let antigen = Antigen::from_str(parts[0])?;
-//         let rh_factor = RhFactor::from_str(parts[1]).expect("Invalid Rh factor");
-
-//         Ok(BloodType { antigen, rh_factor })
-//     }
-// }
-
-impl FromStr for BloodType {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // Check if the string has the correct format
-        if!s.chars().count() == 3 && (s.chars().last()!= Some('+') && s.chars().last()!= Some('-')) {
-            return Err("Invalid blood type format. Expected format: Antigen+/-".to_string());
-        }
-
-        // Split the string into parts
-        let parts: Vec<&str> = s.split("").collect();
-
-        // Parse the antigen and Rh factor
-        let antigen = Antigen::from_str(parts[0])?;
-        let rh_factor = RhFactor::from_str(parts[1]);
-
-        // Handle the possibility of an Err from RhFactor::from_str
-        match rh_factor {
-            Ok(rh) => Ok(BloodType { antigen, rh_factor: rh }),
-            Err(_) => Err("Invalid Rh factor".to_string()),
-        }
-    }
-}
-
-
-
-
-use std::fmt::{self, Debug};
 
 impl Debug for BloodType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let rh_str = match self.rh_factor {
-            RhFactor::Positive => "+",
-            RhFactor::Negative => "-",
-        };
-        write!(f, "{:?}{}", self.antigen, rh_str)
+        write!(f, "{}{}", self.antigen, self.rh_factor)
     }
 }
 
 impl BloodType {
     pub fn can_receive_from(&self, other: &Self) -> bool {
-        match (self.antigen.clone(), other.antigen.clone(), self.rh_factor.clone(), other.rh_factor.clone()) {
-            (Antigen::AB, _, _, _) => true,
-            (Antigen::A, Antigen::A, _, _) => true,
-            (Antigen::A, Antigen::O, _, _) => true,
-            (Antigen::B, Antigen::B, _, _) => true,
-            (Antigen::B, Antigen::O, _, _) => true,
-            (Antigen::O, _, _, _) => true,
-            _ => false,
-        }
+        self.donors().contains(other)
     }
 
     pub fn donors(&self) -> Vec<Self> {
-        let mut donors = Vec::new();
-        let antigen = self.antigen.clone();
-        let rh_factor = self.rh_factor.clone();
-        match (antigen, rh_factor) {
-            (Antigen::A, RhFactor::Positive) => {
-                donors.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Positive });
-                donors.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::A, RhFactor::Negative) => {
-                donors.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::B, RhFactor::Positive) => {
-                donors.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Positive });
-                donors.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::B, RhFactor::Negative) => {
-                donors.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::AB, RhFactor::Positive) => {
-                donors.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Positive });
-                donors.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Negative });
-                donors.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Positive });
-                donors.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Negative });
-                donors.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-                donors.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::AB, RhFactor::Negative) => {
-                donors.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Negative });
-                donors.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Negative });
-                donors.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::O, RhFactor::Positive) => {
-                donors.push(BloodType { antigen: Antigen::O, rh_factor: RhFactor::Positive });
-                donors.push(BloodType { antigen: Antigen::O, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::O, RhFactor::Negative) => {
-                donors.push(BloodType { antigen: Antigen::O, rh_factor: RhFactor::Negative });
-            },
+        match (&self.antigen, &self.rh_factor) {
+            (Antigen::O, RhFactor::Positive) => vec![
+                "O+".parse::<BloodType>().unwrap(),
+                "O-".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::A, RhFactor::Positive) => vec![
+                "A+".parse::<BloodType>().unwrap(),
+                "A-".parse::<BloodType>().unwrap(),
+                "O+".parse::<BloodType>().unwrap(),
+                "O-".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::A, RhFactor::Negative) => vec![
+                "A-".parse::<BloodType>().unwrap(),
+                "O-".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::AB, RhFactor::Positive) => vec![
+                "O+".parse::<BloodType>().unwrap(),
+                "O-".parse::<BloodType>().unwrap(),
+                "A+".parse::<BloodType>().unwrap(),
+                "A-".parse::<BloodType>().unwrap(),
+                "B+".parse::<BloodType>().unwrap(),
+                "B-".parse::<BloodType>().unwrap(),
+                "AB-".parse::<BloodType>().unwrap(),
+                "AB+".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::AB, RhFactor::Negative) => vec![
+                "AB-".parse::<BloodType>().unwrap(),
+                "O-".parse::<BloodType>().unwrap(),
+                "B-".parse::<BloodType>().unwrap(),
+                "A-".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::B, RhFactor::Positive) => vec![
+                "B+".parse::<BloodType>().unwrap(),
+                "B-".parse::<BloodType>().unwrap(),
+                "O+".parse::<BloodType>().unwrap(),
+                "O-".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::B, RhFactor::Negative) => vec![
+                "B-".parse::<BloodType>().unwrap(),
+                "O-".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::O, RhFactor::Negative) => vec!["O-".parse::<BloodType>().unwrap()],
         }
-        donors
     }
 
     pub fn recipients(&self) -> Vec<BloodType> {
-        let mut recipients = Vec::new();
-        let antigen = self.antigen.clone();
-        let rh_factor = self.rh_factor.clone();
-        match (antigen, rh_factor) {
-            (Antigen::A, RhFactor::Positive) => {
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Positive });
-            },
-            (Antigen::A, RhFactor::Negative) => {
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Negative });
-                recipients.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::B, RhFactor::Positive) => {
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Positive });
-            },
-            (Antigen::B, RhFactor::Negative) => {
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Negative });
-                recipients.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::AB, RhFactor::Positive) => {
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-            },
-            (Antigen::AB, RhFactor::Negative) => {
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Negative });
-            },
-            (Antigen::O, RhFactor::Positive) => {
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::O, rh_factor: RhFactor::Positive });
-            },
-            (Antigen::O, RhFactor::Negative) => {
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::AB, rh_factor: RhFactor::Negative });
-                recipients.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::A, rh_factor: RhFactor::Negative });
-                recipients.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::B, rh_factor: RhFactor::Negative });
-                recipients.push(BloodType { antigen: Antigen::O, rh_factor: RhFactor::Positive });
-                recipients.push(BloodType { antigen: Antigen::O, rh_factor: RhFactor::Negative });
-            },
+        match (&self.antigen, &self.rh_factor) {
+            (Antigen::O, RhFactor::Positive) => vec![
+                "O+".parse::<BloodType>().unwrap(),
+                "A+".parse::<BloodType>().unwrap(),
+                "B+".parse::<BloodType>().unwrap(),
+                "AB+".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::A, RhFactor::Positive) => vec![
+                "A+".parse::<BloodType>().unwrap(),
+                "AB+".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::A, RhFactor::Negative) => vec![
+                "A-".parse::<BloodType>().unwrap(),
+                "A+".parse::<BloodType>().unwrap(),
+                "AB+".parse::<BloodType>().unwrap(),
+                "AB-".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::AB, RhFactor::Positive) => vec!["AB+".parse::<BloodType>().unwrap()],
+            (Antigen::AB, RhFactor::Negative) => vec![
+                "AB-".parse::<BloodType>().unwrap(),
+                "AB+".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::B, RhFactor::Positive) => vec![
+                "B+".parse::<BloodType>().unwrap(),
+                "AB+".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::B, RhFactor::Negative) => vec![
+                "B-".parse::<BloodType>().unwrap(),
+                "B+".parse::<BloodType>().unwrap(),
+                "AB+".parse::<BloodType>().unwrap(),
+                "AB-".parse::<BloodType>().unwrap(),
+            ],
+            (Antigen::O, RhFactor::Negative) => vec![
+                "O+".parse::<BloodType>().unwrap(),
+                "O-".parse::<BloodType>().unwrap(),
+                "A+".parse::<BloodType>().unwrap(),
+                "A-".parse::<BloodType>().unwrap(),
+                "B+".parse::<BloodType>().unwrap(),
+                "B-".parse::<BloodType>().unwrap(),
+                "AB-".parse::<BloodType>().unwrap(),
+                "AB+".parse::<BloodType>().unwrap(),
+            ],
         }
-        recipients
     }
 }
