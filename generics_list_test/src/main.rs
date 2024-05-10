ae99fe547b82578d3b54b81e7260e7253d68f149
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-10 18:15:35 +0000

    Piscine Rust

--- generics_list_test/src/main.rs
+++ generics_list_test/src/main.rs
@@ -13,6 +13,9 @@ fn main() {
     println!("The size of the list is {}", new_list_str.len());
 }
 
+
+
+
 #[derive(Clone, Debug)]
 pub struct List<T> {
     pub head: Option<Node<T>>,
@@ -27,12 +30,27 @@ pub struct Node<T> {
 
 impl<T> List<T> {
     pub fn new() -> List<T> {
+        List {
+            head: None,
+            size: 0,
+        }
     }
 
     pub fn push(&mut self, value: T) {
+        self.size += 1;
+        let new_node = Node {
+            value: value,
+            next: self.head.take().map(Box::new),
+        };
+
+        self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) {
+        if let Some(node) = self.head.take() {
+            self.size -= 1;
+            self.head = node.next.map(|boxed_node| *boxed_node);
+        }
     }
 
     pub fn len(&self) -> usize {
--- roman_numbers/src/lib.rs
+++ roman_numbers/src/lib.rs
@@ -1,4 +1,3 @@
-
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum RomanDigit {
     Nulla,
@@ -15,40 +14,39 @@ pub enum RomanDigit {
 pub struct RomanNumber(pub Vec<RomanDigit>);
 
 impl From<u32> for RomanDigit {
-    fn from(num: u32) -> Self {
-        match num {
-            0 => RomanDigit::Nulla,
-            1 => RomanDigit::I,
-            5 => RomanDigit::V,
-            10 => RomanDigit::X,
-            50 => RomanDigit::L,
-            100 => RomanDigit::C,
-            500 => RomanDigit::D,
-            1000 => RomanDigit::M,
-            _ => panic!(),
+     fn from(n:u32)-> Self{
+        match n{
+            1=> RomanDigit::I,
+            _=>RomanDigit::Nulla
         }
     }
-}
+} 
 
 impl From<u32> for RomanNumber {
-    fn from(mut num: u32) -> Self{
-        if num == 0 {
-            return RomanNumber(vec![RomanDigit::Nulla]);
-        }
-        
-        let mut result = Vec::new();
-        let div = vec![1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-        
-        for (i, n) in div.iter().enumerate() {
-            while n <= &num {
-                if i % 2 == 0 {
-                    result.push(RomanDigit::from(*n));
-                }else{
-                    let rem = div[i - 1] - div[i];
-                    result.push(RomanDigit::from(rem));
-                    result.push(RomanDigit::from(div[i - 1]));
-                }
-                num -= n;
+
+     fn from(n:u32)-> Self{
+        let conversions = [
+            (1000, vec![RomanDigit::M]),
+            (900, vec![RomanDigit::C, RomanDigit::M]),
+            (500, vec![RomanDigit::D]),
+            (400, vec![RomanDigit::C, RomanDigit::D]),
+            (100, vec![RomanDigit::C]),
+            (90, vec![RomanDigit::X, RomanDigit::C]),
+            (50, vec![RomanDigit::L]),
+            (40, vec![RomanDigit::X, RomanDigit::L]),
+            (10, vec![RomanDigit::X]),
+            (9, vec![RomanDigit::I, RomanDigit::X]),
+            (5, vec![RomanDigit::V]),
+            (4, vec![RomanDigit::I, RomanDigit::V]),
+            (1, vec![RomanDigit::I]),
+        ];
+
+        let mut result :Vec<RomanDigit> =  Vec::new();
+        let mut i =n;
+        for v in conversions.iter(){
+            while i >= v.0{
+                result.extend(&v.1);
+               i= i-v.0;
             }
         }
         RomanNumber(result)
