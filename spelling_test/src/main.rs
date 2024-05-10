dd10d33cb9d722b0dcfb3bea045f52577aa4e689
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-10 10:54:10 +0000

    Piscine Rust

--- spelling_test/src/main.rs
+++ spelling_test/src/main.rs
@@ -4,69 +4,66 @@ pub fn spell(n: u64) -> String {
     let tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
     let thousands = ["thousand", "million"];
 
-    // ZERO
+    // zero
     if n == 0 {
         return "zero".to_string();
     }
 
-    // UNITÉ
+    // unite
     if n < 10 {
         return ones[n as usize].to_string();
     }
 
-    // DIZAINE
+    // dizaine
     if n < 20 {
         return teens[(n - 10) as usize].to_string();
     }
 
-    // CENTAINE
+    // centaine
     if n < 100 {
-        let unite : usize = n as usize % 10;
-        let dizaine : usize = n as usize / 10;
-        
-        if unite != 0{
-            return format!("{}-{}", tens[dizaine].to_string(), ones[unite].to_string());
+        let quotient : u64 = n/10;
+        let reste : u64 = n%10;
+
+        if reste != 0{
+            return format!("{}-{}", tens[quotient as usize], ones[reste as usize]);
         } else {
-            return format!("{}", tens[dizaine].to_string());   
+            return format!("{}", tens[quotient as usize]);
         }
     }
 
-    // MILLIÉME
-    if n < 1000 {
-        let hundreds = n / 100;
-        let remainder = n % 100;
-
-        if remainder != 0{
-            return format!("{} hundred {}", ones[hundreds as usize], spell(remainder));
+    // millier && million
+    if n < 1000{
+        let quotient : u64 = n/100;
+        let reste : u64 = n%100;
+        if reste == 0 {
+            return format!("{} hundred", spell(quotient));
+        }else{
+            return format!("{} hundred {}", spell(quotient), spell(reste));
         }
-
-        return format!("{} hundred", ones[hundreds as usize]);
-    }
-
-    
-    // MILLION
-    for (index, &thousand) in thousands.iter().enumerate().rev() {
-        let base = 10u64.pow((index as u32 + 1) * 3);
-        if n >= base {
-            let prefix = spell(n / base);
-            let suffix = if n % base == 0 {
-                "".to_string()
-            } else {
-                format!(" {}", spell(n % base))
-            };
-            return format!("{} {}{}", prefix, thousand, suffix);
+    } else {
+        for (index, &thousand) in thousands.iter().enumerate().rev() {
+            let base = 10u64.pow((index as u32 + 1) * 3);
+            if n >= base {
+                let prefix = spell(n / base);
+                let suffix = if n % base == 0 {
+                    "".to_string()
+                } else {
+                    format!(" {}", spell(n % base))
+                };
+                return format!("{} {}{}", prefix, thousand, suffix);
+            }
         }
+        unreachable!(); 
     }
-    unreachable!(); 
 }
 
-
 fn main() {
-    println!("{}", spell(0));
-    println!("{}", spell(8));
-    println!("{}", spell(18));
-    println!("{}", spell(48));
-
-    println!("{}", spell(348));
-    println!("{}", spell(9996));
+    println!("{}: {}", 0, spell(0));
+    println!("{}: {}", 8, spell(8));
+    println!("{}: {}", 10, spell(10));
+    println!("{}: {}", 20, spell(20));
+    println!("{}: {}", 48,  spell(48));
+    println!("{}: {}", 100, spell(100));
+    println!("{}: {}", 348, spell(348));
+    println!("{}: {}", 9996, spell(9996));
 }
