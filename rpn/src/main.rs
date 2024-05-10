e3346f5717f81e5558ba809127ddf89c5b734177
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-10 15:27:59 +0000

    Piscine Rust

--- rpn/src/main.rs
+++ rpn/src/main.rs
@@ -69,6 +69,5 @@ pub fn rpn(expr: &str) {
     }
 }
 
-fn main() {
-    
+fn main() {   
 }
--- rpn_test/src/main.rs
+++ rpn_test/src/main.rs
@@ -4,8 +4,76 @@ fn main() {
     rpn(&args[1]);
 }
 
-pub fn rpn(expr: &str) {
-    let mut stack : Vec<i64> = Vec::new();
 
-    
+pub fn rpn(expr: &str) {
+    let mut result : Vec<i64> = Vec::new();
+
+    for token in expr.split_whitespace() {
+        match token {
+            "+" => {
+                if let (Some(b), Some(a)) = (result.pop(), result.pop()){
+                    result.push(a + b);
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            "-" => {
+                if let (Some(b), Some(a)) = (result.pop(), result.pop()){
+                    result.push(a - b);
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            "*" => {
+                if let (Some(b), Some(a)) = (result.pop(), result.pop()){
+                    result.push(a * b);
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            "/" => {
+                if let (Some(b), Some(a)) = (result.pop(), result.pop()){
+                    if b != 0 {
+                        result.push(a / b);
+                    } else {
+                        println!("Error");
+                        return;
+                    }
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            "%" => {
+                if let (Some(b), Some(a)) = (result.pop(), result.pop()){
+                    if b != 0 {
+                        result.push(a % b);
+                    } else {
+                        println!("Error");
+                        return;
+                    }
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            _ => {
+                if let Ok(num) = token.parse::<i64>() {
+                    result.push(num);
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+        }
+    }
+
+    if result.len() == 1 {
+        println!("{}", result.pop().unwrap());
+    } else {
+        println!("Error");
+    }
 }
