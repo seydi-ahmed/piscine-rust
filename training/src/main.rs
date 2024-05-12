f6510c1eaa0eb8616f8b6707d097e13101ab7cf7
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-12 21:02:45 +0000

    Piscine Rust

--- training/src/main.rs
+++ training/src/main.rs
@@ -1,51 +1,26 @@
-pub fn capitalize_first(input: &str) -> String {
-    let mut res : String = String::new();
-    let mut i = 0;
+use std::collections::HashMap;
 
-    for elem in input.chars() {
-        if i == 0 {
-            res.push_str(&elem.to_uppercase().to_string());
-            i += 1;
-        } else {
-            res.push_str(&elem.to_string());
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    let mut max : i32 = 0;
+
+    for (_, elem) in h {
+        if elem > max {
+            max = elem;
         }
     }
 
-    res
+
+    max
 }
-
-pub fn title_case(input: &str) -> String {
-    let mut res : String = String::new();
-
-    for elem in input.split_whitespace() {
-        res.push_str(&capitalize_first(elem));
-        res.push_str(&" ".to_string());
-    }
-
-    res.trim().to_string()
-}
-
-pub fn change_case(input: &str) -> String {
-    let mut res : String = String::new();
-
-    for elem in input.chars() {
-        if elem.is_uppercase() {
-            res.push_str(&elem.to_lowercase().to_string());
-        } else if elem.is_lowercase() {
-            res.push_str(&elem.to_uppercase().to_string());
-        } else {
-            res.push_str(&elem.to_string());
-        }
-    }
-
-    res
-}
-
-
 
 
 fn main() {
-    println!("{}", capitalize_first("joe is missing"));
-    println!("{}", title_case("jill is leaving A"));
-    println!("{}",change_case("heLLo THere"));
+
+    let mut hash = HashMap::new();
+    hash.insert("Daniel", 122);
+    hash.insert("Ashley", 333);
+    hash.insert("Katie", 334);
+    hash.insert("Robert", 14);
+
+    println!("The biggest of the elements in the HashMap is {}", bigger(hash));
 }
