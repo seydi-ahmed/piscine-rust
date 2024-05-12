db25288d83c88f439b57fbd02cef133998faf253
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-12 20:34:03 +0000

    Piscine Rust

--- training/src/main.rs
+++ training/src/main.rs
@@ -1,28 +1,38 @@
-fn inv_pyramid(v: String, i: u32) -> Vec<String> {
-    (1..=i as usize)
-        .chain((1..i as usize).rev())
-        .map(|j| format!("{}{}", " ".repeat(j), v.repeat(j)))
-        .collect()
+pub fn nbr_function(c: i32) -> (i32, f64, f64) {
+    (c, (c as f64).exp(), (c as f64).abs().ln())
+}
+
+pub fn str_function(a: String) -> (String, String) {
+    let mut aa : String = String::new();
+
+    for elem in a.split_whitespace() {
+        let mut res : f64 = elem.parse().unwrap();
+        res = res.exp();
+        aa.push_str(&res.to_string());
+        aa.push_str(&" ".to_string());
+    }
+    (a, aa.trim().to_string())
+}
+
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    let bb : Vec<f64> = Vec::new();
+
+    for elem in b {
+        let res = (elem as f64).abs().ln();
+        bb.push(res);
+    }
+
+    (b, bb)
 }
 
 
 
 fn main() {
-    let a = inv_pyramid(String::from("#"), 1);
-    let b = inv_pyramid(String::from("a"), 2);
-    let c = inv_pyramid(String::from(">"), 5);
-    let d = inv_pyramid(String::from("&"), 8);
+    let a: i32 = 0;
+    let b = String::from("1 2 4 5 6");
+    let c = vec![1, 2, 4, 5];
 
-    for v in a.iter() {
-        println!("{:?}", v);
-    }
-    for v in b.iter() {
-        println!("{:?}", v);
-    }
-    for v in c.iter() {
-        println!("{:?}", v);
-    }
-    for v in d.iter() {
-        println!("{:?}", v);
-    }
+    println!("{:?}", nbr_function(a));
+    println!("{:?}", str_function(b));
+    println!("{:?}", vec_function(c));
 }
