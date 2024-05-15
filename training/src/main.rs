79e4112452337df6b6aba44ab2467f165cf3d962
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-15 18:19:14 +0000

    Piscine Rust

--- training/src/main.rs
+++ training/src/main.rs
@@ -1,37 +1,65 @@
-use crate::RomanDigit::*;
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum RomanDigit {
-	Nulla,
-	I,
-	V,
-	X,
-	L,
-	C,
-	D,
-	M,
-}
-
-#[derive(Debug, Clone, Debug, PartialEq, Eq)]
-pub struct RomanNumber(pub Vec<RomanDigit>);
-
-impl From<u32> for RomanDigit {
-    fn from(num: u32) -> RomanDigit {
-        match num {
-            
+pub mod library {
+    pub mod writers {
+        use super::books::Book;
+        pub struct Writer {
+            pub first_name: String,
+            pub last_name: String,
+            pub books: Vec<Book>
         }
     }
+    
+    pub mod books {
+        pub struct Book {
+            pub title: String,
+            pub year: u64
+        }
+    }
+    
 }
 
-impl From<u32> for RomanNumber {
+use crate::library::writers::Writer;
+pub fn order_books(writer: &mut Writer) {
+    writer.books.sort_by_key(|a| a.title.clone());
 }
 
 
 // ***************************************************************************
 
+
+pub use library::books::Book;
 fn main() {
-	println!("{:?}", RomanNumber::from(32));
-	println!("{:?}", RomanNumber::from(9));
-	println!("{:?}", RomanNumber::from(45));
-	println!("{:?}", RomanNumber::from(0));
+    let mut writer_a = Writer {
+        first_name: "William".to_string(),
+        last_name: "Shakespeare".to_string(),
+        books: vec![
+            Book {
+                title: "Hamlet".to_string(),
+                year: 1600,
+            },
+            Book {
+                title: "Othelo".to_string(),
+                year: 1603,
+            },
+            Book {
+                title: "Romeo and Juliet".to_string(),
+                year: 1593,
+            },
+            Book {
+                title: "MacBeth".to_string(),
+                year: 1605,
+            },
+        ],
+    };
+
+    println!("Before ordering");
+    for b in &writer_a.books {
+        println!("{:?}", b.title);
+    }
+
+    order_books(&mut writer_a);
+
+    println!("\nAfter ordering");
+    for b in writer_a.books {
+        println!("{:?}", b.title);
+    }
 }
