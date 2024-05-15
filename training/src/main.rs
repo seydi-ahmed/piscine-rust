840ed9d75d1e4c354f88f92509145984f192fb47
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-05-15 19:12:14 +0000

    Piscine Rust

--- training/src/main.rs
+++ training/src/main.rs
@@ -1,65 +1,39 @@
-pub mod library {
-    pub mod writers {
-        use super::books::Book;
-        pub struct Writer {
-            pub first_name: String,
-            pub last_name: String,
-            pub books: Vec<Book>
+pub fn rot21(input: &str) -> String {
+    let mut res : String = String::new();
+
+    for letter in input.chars(){
+        if letter >= 'a' && letter <= 'z'{
+            let letter_to_pushed = rotate_lowercase(letter, 21);
+            res.push_str(&letter_to_pushed.to_string());
+        } else if letter >= 'A' && letter <= 'Z'{
+            let letter_to_pushed = rotate_uppercase(letter, 21);
+            res.push_str(&letter_to_pushed.to_string());
+        } else {
+            res.push_str(&letter.to_string());
         }
     }
-    
-    pub mod books {
-        pub struct Book {
-            pub title: String,
-            pub year: u64
-        }
-    }
-    
+
+    res
 }
 
-use crate::library::writers::Writer;
-pub fn order_books(writer: &mut Writer) {
-    writer.books.sort_by_key(|a| a.title.clone());
+pub fn rotate_lowercase(c: char, shift: u8) -> char {
+    let base = b'a';
+    let c_rotated = ((c as u8 - base + shift) % 26) + base;
+    c_rotated as char
 }
 
+pub fn rotate_uppercase(c: char, shift: u8) -> char {
+    let base = b'A';
+    let c_rotated = ((c as u8 - base + shift) % 26) + base;
+    c_rotated as char
+}
 
 // ***************************************************************************
 
-
-pub use library::books::Book;
 fn main() {
-    let mut writer_a = Writer {
-        first_name: "William".to_string(),
-        last_name: "Shakespeare".to_string(),
-        books: vec![
-            Book {
-                title: "Hamlet".to_string(),
-                year: 1600,
-            },
-            Book {
-                title: "Othelo".to_string(),
-                year: 1603,
-            },
-            Book {
-                title: "Romeo and Juliet".to_string(),
-                year: 1593,
-            },
-            Book {
-                title: "MacBeth".to_string(),
-                year: 1605,
-            },
-        ],
-    };
-
-    println!("Before ordering");
-    for b in &writer_a.books {
-        println!("{:?}", b.title);
-    }
-
-    order_books(&mut writer_a);
-
-    println!("\nAfter ordering");
-    for b in writer_a.books {
-        println!("{:?}", b.title);
-    }
+    println!("The letter \"a\" becomes: {}", rot21("a"));
+    println!("The letter \"m\" becomes: {}", rot21("m"));
+    println!("The word \"MISS\" becomes: {}", rot21("MISS"));
+    println!("Your cypher wil be: {}", rot21("Testing numbers 1 2 3"));
+    println!("Your cypher wil be: {}", rot21("rot21 works!"));
 }
